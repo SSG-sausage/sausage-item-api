11549d80edbf69cf044085050a9172ffb078f6af
JeongSangByuk <jsb100800@naver.com>
2023-01-30 09:46:09 +0900

    [TERK-103/Fix] 배송타입명 수정

--- src/main/java/com/ssg/sausageitemapi/item/entity/ShppCd.java
+++ src/main/java/com/ssg/sausageitemapi/item/entity/ShppCd.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum ShppCd {
 
     SSG_SHPP("쓱배송"),
-    EMART_TRADERS_SHPP("이마트 트레이더스 배송");
+    SSG_TRADERS_SHPP("쓱배송 트레이더스 배송");
 
     private final String nm;
 }
